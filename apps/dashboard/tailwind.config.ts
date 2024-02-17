1246201738e9b8886a927932d81ee8a3f070d818
Terris Kremer <terris.kremer@gmail.com>
2024-02-16 21:12:23 -0500

    fic tailwind config in apps

--- apps/dashboard/tailwind.config.ts
+++ apps/dashboard/tailwind.config.ts
@@ -3,9 +3,10 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@config/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets" | "important"> = {
   content: ["./app/**/*.tsx", "./lib/**/*.tsx"],
   presets: [sharedConfig],
+  important: true,
 };
 
 export default config;
